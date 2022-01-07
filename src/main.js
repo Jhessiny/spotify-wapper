54d711b1cda7560b75b16abede684a9ae2a82926
jhessiny <jhessinymattos@gmail.com>
2022-01-07 12:11:29 -0300

    test husky

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-const a = 1;
+export const a = 1;
+
+export function ab() {
+	return a + 3;
+}
+
+// ab();
---
+++ tests/main.spec.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { a } from "../src/main";
+
+describe("oi", function () {
+	it("oi", function () {
+		expect(a).to.exist;
+	});
+});
